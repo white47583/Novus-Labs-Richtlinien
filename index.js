32d7629ae9713a917332b91f118318c06e879335
white <106684743+white47583@users.noreply.github.com>
2022-06-10 20:46:29 +0200

    Pre Release 10.06.2022
    Information : 
    Es wurden einige Funktionen noch nicht hinzugefügt und diese Version enthält noch Fehler
    HERUNTERLADEN AUF EIGENE GEFAHR

---
+++ index.js
@@ -0,0 +1,158 @@
+function search() {
+ 
+    let searcheingabe = document.getElementById('waterfall-exp').value;
+   if (isNaN(searcheingabe)) {
+    alert("Bitte gebe nur die Nummer des Artikels ein den du suchst. Wie 1 (zu Artikel 1)");
+   }
+
+
+    else if(searcheingabe == 1 ) {
+      var target = document.getElementById("artikel1");
+
+      target.scrollIntoView({block: "start", behavior: "smooth"});
+      let input = document.getElementById('artikel1').style.backgroundColor="yellow";
+      let input2 =document.getElementById('artikel1').style.color="black";
+    }
+    else if(searcheingabe == 2 ) {
+      var target = document.getElementById("artikel2");
+      target.scrollIntoView({block: "start", behavior: "smooth"});
+      let input = document.getElementById('artikel2').style.backgroundColor="yellow";
+
+    }
+    else if(searcheingabe == 3 ) {
+      var target = document.getElementById("artikel3")
+      target.scrollIntoView({block: "start", behavior: "smooth"});
+      let input = document.getElementById('artikel3').style.backgroundColor="yellow";
+    }
+
+    else if(searcheingabe == 4 ) {
+      var target = document.getElementById("artikel4")
+      target.scrollIntoView({block: "start", behavior: "smooth"});
+      let input = document.getElementById('artikel4').style.backgroundColor="yellow";
+    }
+
+    else if(searcheingabe == 5 ) {
+      var target = document.getElementById("artikel5")
+      target.scrollIntoView({block: "start", behavior: "smooth"});
+      let input = document.getElementById('artikel5').style.backgroundColor="yellow";
+    }
+
+    else if(searcheingabe == 6 ) {
+      var target = document.getElementById("artikel6")
+      target.scrollIntoView({block: "start", behavior: "smooth"});
+      let input = document.getElementById('artikel6').style.backgroundColor="yellow";
+    }
+
+    else if(searcheingabe == 7 ) {
+      var target = document.getElementById("artikel7")
+      target.scrollIntoView({block: "start", behavior: "smooth"});
+      let input = document.getElementById('artikel7').style.backgroundColor="yellow";
+    }
+
+    else if(searcheingabe == 8 ) {
+      var target = document.getElementById("artikel8")
+      target.scrollIntoView({block: "start", behavior: "smooth"});
+      let input = document.getElementById('artikel8').style.backgroundColor="yellow";
+    }
+
+    else if(searcheingabe == 9 ) {
+      var target = document.getElementById("artikel9")
+      target.scrollIntoView({block: "start", behavior: "smooth"});
+      let input = document.getElementById('artikel9').style.backgroundColor="yellow";
+    }
+
+    else if(searcheingabe == 10 ) {
+      var target = document.getElementById("artikel10")
+      target.scrollIntoView({block: "start", behavior: "smooth"});
+      let input = document.getElementById('artikel10').style.backgroundColor="yellow";
+    }
+
+    else if(searcheingabe == 11 ) {
+      var target = document.getElementById("artikel11")
+      target.scrollIntoView({block: "start", behavior: "smooth"});
+      let input = document.getElementById('artikel11').style.backgroundColor="yellow";
+    }
+
+    else if(searcheingabe == 12 ) {
+      var target = document.getElementById("artikel12")
+      target.scrollIntoView({block: "start", behavior: "smooth"});
+      let input = document.getElementById('artikel12').style.backgroundColor="yellow";
+    }
+
+    else if(searcheingabe == 13 ) {
+      var target = document.getElementById("artikel13")
+      target.scrollIntoView({block: "start", behavior: "smooth"});
+      let input = document.getElementById('artikel13').style.backgroundColor="yellow";
+    }
+
+    else if(searcheingabe == 14 ) {
+      var target = document.getElementById("artikel14")
+      target.scrollIntoView({block: "start", behavior: "smooth"});
+      let input = document.getElementById('artikel14').style.backgroundColor="yellow";
+    }
+
+    else if(searcheingabe == 15 ) {
+      var target = document.getElementById("artikel15")
+      target.scrollIntoView({block: "start", behavior: "smooth"});
+      let input = document.getElementById('artikel15').style.backgroundColor="yellow";
+    }
+
+    else if(searcheingabe == 16 ) {
+      var target = document.getElementById("artikel16")
+      target.scrollIntoView({block: "start", behavior: "smooth"});
+      let input = document.getElementById('artikel16').style.backgroundColor="yellow";
+    }
+    else if(searcheingabe == 17 ) {
+      var target = document.getElementById("artikel17")
+      target.scrollIntoView({block: "start", behavior: "smooth"});
+      let input = document.getElementById('artikel17').style.backgroundColor="yellow";
+    }
+
+    else if(searcheingabe == 18 ) {
+      var target = document.getElementById("artikel18")
+      target.scrollIntoView({block: "start", behavior: "smooth"});
+      let input = document.getElementById('artikel18').style.backgroundColor="yellow";
+    }
+    else if(searcheingabe == 19 ) {
+      var target = document.getElementById("artikel19")
+      target.scrollIntoView({block: "start", behavior: "smooth"});
+      let input = document.getElementById('artikel19').style.backgroundColor="yellow";
+    }
+
+    else if(searcheingabe == 20 ) {
+      var target = document.getElementById("artikel20")
+      target.scrollIntoView({block: "start", behavior: "smooth"});
+      let input = document.getElementById('artikel20').style.backgroundColor="yellow";
+    }
+
+    else if(searcheingabe == 21 ) {
+      var target = document.getElementById("artikel21")
+      target.scrollIntoView({block: "start", behavior: "smooth"});
+      let input = document.getElementById('artikel21').style.backgroundColor="yellow";
+    }
+    
+    else if(searcheingabe == 22 ) {
+      var target = document.getElementById("artikel22")
+      target.scrollIntoView({block: "start", behavior: "smooth"});
+      let input = document.getElementById('artikel22').style.backgroundColor="yellow";
+    }
+
+    else if(searcheingabe == 23 ) {
+      var target = document.getElementById("artikel23")
+      target.scrollIntoView({block: "start", behavior: "smooth"});
+      let input = document.getElementById('artikel23').style.backgroundColor="yellow";
+    }
+
+
+
+    else if(searcheingabe > 23) {
+    alert("Diesen Artkel gibt es nicht");
+    }
+  }
+
+  
+
+ 
+
+
+
